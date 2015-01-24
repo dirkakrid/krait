8e820493d5b8f5c4540125dc60cf5777be17ed01
Tyler Christiansen <tyler@oss-stack.io>
2015-01-23 19:51:49 -0800

    fix landscapes complaints

--- lib/router.py
+++ lib/router.py
@@ -1,23 +1,59 @@
+"""Router Module
+
+This module implements some abstraction to allow users to interact more
+naturally with Junos devices via the Python interpreter.
+
+Most of the methods default to a format of "text" for the RPC calls.  This is
+because this is designed primarily for traditional network engineers, and
+that's the output format they'll be most comfortable with.
+
+With the above said, although this module is primarily for interacting with
+Junos devices via the Python interpreter, it can also be used in a traditional
+programming or scripting environment.  For that purpose, it is highly
+recommended that you override the default format and set it to "xml".
+
+"""
 from __future__ import print_function
 from jnpr.junos import Device
 from lxml import etree
 
 
 class Router(object):
+    """Router Class
+
+    Implement the Router object.  Routers are the most common network
+    elements, and we focus on these and their capabilities.  Most of this
+    class's methods have a `format` that defaults to `text`.  It can be
+    specified as `xml` for a more programattic interface to the devices.
+
+    """
+
     @property
     def host(self):
+        """
+        returns the hostname
+        """
         return self._host
 
     @property
     def user(self):
+        """
+        returns the user used to login
+        """
         return self._user
 
     @property
     def connected(self):
+        """
+        returns the state of the connection
+        """
         return self._connected
 
     @property
     def connection(self):
+        """
+        returns the connection
+        """
         return self._connection
 
     def __init__(self, **kwargs):
@@ -29,6 +65,11 @@ class Router(object):
         self._connection = self.connect()
 
     def connect(self):
+        """
+        Connect to a device.  Uses some settings specified when a Router
+        object is instantiated.  Also sets `gather_facts`, which is `False`
+        by default (set when the Router object is instantiated).
+        """
         connection = Device(host=self._host,
                             user=self._user,
                             password=self._password,
@@ -39,6 +80,12 @@ class Router(object):
         return connection
 
     def get_config(self, section=None, structure="text"):
+        """
+        Returns the device configuration in `text` format (the format you
+        normally see when typing `show configuration`).  Format can be set to
+        `xml`.  Also accepts a `section`, which is the first level of
+        configuration hierarchy (such as `show configuration protocols`).
+        """
         root = None
         if section is not None:
             root = etree.Element("configuration")
@@ -46,6 +93,11 @@ class Router(object):
         return self._connection.rpc.get_config(root, dict(format=structure))
 
     def get_interfaces(self, *args, **kwargs):
+        """
+        Returns the status of all interfaces.  An individual interface can be
+        specified via the `name` keyword (or positional argument in position
+        `0`).  `format` can also be `xml`.
+        """
         structure = kwargs.get("format", "text")
         name = args[0] if len(args) else kwargs.get("name", None)
         options = {"format": structure}
@@ -54,12 +106,23 @@ class Router(object):
         return self._connection.rpc.get_interface_information(options)
 
     def get_interface(self, *args, **kwargs):
+        """
+        Returns the status of an individual interface.  Similar to
+        `get_interfaces`.  Currently requires `options` to be set to a dict.
+        This should (currently) only be used internally by `get_interfaces`,
+        but it will later be ready for direct access.
+        """
         options = kwargs.get("options")
         name = args[0] if len(args) else kwargs.get("name")
         return self._connection.rpc.get_interface_information(options,
                                                               interface_name=name)
 
     def get_bgp(self, *args, **kwargs):
+        """
+        returns the status of BGP.  May return summary information, all
+        neighbors, or specific neighbors.  Default `format` is `text`, but may
+        be set to `xml` for more programmatic uses.
+        """
         structure = kwargs.get("format", "text")
         neighbor = args[0] if len(args) else kwargs.get("neighbor", None)
         options = {"format": structure}
@@ -71,12 +134,23 @@ class Router(object):
         return self._connection.rpc.get_bgp_neighbor_information(options)
 
     def get_bgp_neighbor(self, *args, **kwargs):
+        """
+        Returns the status of a specific BGP neighbor.  Currently for use by
+        internal methods only.
+        """
         options = kwargs.get("options")
         neighbor = args[0] if len(args) else kwargs.get("neighbor")
         return self._connection.rpc.get_bgp_neighbor_information(options,
                                                                  neighbor_address=neighbor)
 
     def get_stateful_policies(self, *args, **kwargs):
+        """
+        Returns the stateful policies configured on the device.  This is
+        applicable to SRX devices only.  `from` and `to` may be specified as
+        zones to filter down the policies you're interested in.  `detail` is
+        set to `True` by default (but can be set to `False`) to provide better
+        information than just object names.
+        """
         detail = args[0] if len(args) else kwargs.get("detail", True)
         structure = kwargs.get("format", "text")
         options = {"format": structure}
@@ -91,5 +165,9 @@ class Router(object):
                                                           **restrictions)
 
     def get_header(self):
+        """
+        Returns the device name and software version.  It is the equivalent of
+        `show version brief`.
+        """
         return self._connection.rpc.get_software_information({"format": "text"},
                                                              brief=True)
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+This is the main point of entry.  It does some really ugly things to provide
+end users with the most convenience.  It sets a global `SETTINGS` constant
+so that users don't need to remember to pass settings or configuration when
+creating new connections to new devices.
+"""
 from __future__ import print_function
 import lib.router as router
 import argparse
@@ -7,21 +13,35 @@ from lxml import etree
 
 
 def parse_arguments():
+    """
+    Returns parsed CLI arguments.
+    """
     arg_parser = argparse.ArgumentParser(description="Router Power Shell")
     arg_parser.add_argument("-d", "--device", help="Router hostname")
     return arg_parser.parse_args()
 
 def parse_config():
+    """
+    Return the settings specified in `settings_file`.
+
+    """
     settings_file = 'etc/settings.yml'
     with open(settings_file) as fname:
         return yaml.load(fname)
 
 def connect(host):
+    """
+    Returns a router object, which should also connect the user to the router.
+    """
     return router.Router(host=host,
-                         user=settings['credentials']['username'],
-                         password=settings['credentials']['password'])
+                         user=SETTINGS['credentials']['username'],
+                         password=SETTINGS['credentials']['password'])
 
 def main():
+    """
+    Main point of entry for the script.  Parses arguments, creates
+    connections, and creates an interactive Python interpreter.
+    """
     args = parse_arguments()
     print("Krait Power Shell for Junos")
     print("Author: Tyler Christiansen <code@tylerc.me>")
@@ -37,5 +57,5 @@ def main():
 
 # Ugly globalness to allow users to call connect more effortlessly from
 # the shell
-settings = parse_config()
+SETTINGS = parse_config()
 main()
